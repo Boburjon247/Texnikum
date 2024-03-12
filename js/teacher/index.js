bec37d6438d13b720b9483a9e19e24a4f1ec782a
Boburjon247 <abdunazarov247>
2024-03-12 16:55:38 +0500

    test

--- js/teacher/index.js
+++ js/teacher/index.js
@@ -1,13 +1,40 @@
-document.addEventListener('DOMContentLoaded', function () {
-     const btn = document.querySelector('#btn');
-     const selectID1 = document.querySelectorAll('.selectID');
-     const table1 = document.querySelector('.table1');
-     btn.addEventListener('click', (e) => {
-          e.preventDefault();
-          selectID1.forEach((element, index) => {
-               if (element.value != '') {
-                    table1.classList.add('show')
-               }
-          });
-     })
+const btn = document.querySelector('#btn');
+const selectID1 = document.querySelectorAll('.selectID');
+const table1 = document.querySelector('.table1');
+btn.addEventListener('click', (e) => {
+     e.preventDefault();
+     selectID1.forEach((element, index) => {
+          if (element.value != '') {
+               table1.classList.add('show')
+          }
+     });
+})
+// tabs
+const button_item = document.querySelectorAll(".button_item li");
+const tabPanel = document.querySelectorAll(".tabPanel");
+
+function show(index, color) {
+     console.log(index);
+     button_item.forEach(Element => {
+          Element.style.backgroundColor = '';
+          Element.style.color = '';
+     });
+     button_item[index].style.backgroundColor = color;
+     button_item[index].style.color = 'white';
+
+     tabPanel.forEach(Element => {
+          Element.style.display = 'none';
+     });
+
+     tabPanel[index].style.backgroundColor = color;
+     tabPanel[index].style.display = 'block';
+
+
+}
+button_item.forEach((element, index) => {
+     element.addEventListener('click', () => {
+          localStorage.setItem('tabPanel', index)
+     });
 });
+show(localStorage.getItem('tabPanel'), '#FFE4C4')
+   
